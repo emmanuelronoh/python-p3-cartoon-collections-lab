aaad71cc653ab54903bb9ffcec06cb7cc0e2df2f
emmanuel ronoh <eronoh036@gmail.com>
2024-09-09 16:51:39 +0300

    first commit

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+# cartoon_collections.py
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarves):
+    '''Prints out the 7 dwarfs in a numbered list.'''
+    for index, dwarf in enumerate(dwarves, start=1):
+        print(f"{index}. {dwarf}")
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(elements):
+    '''Capitalizes each element and adds an exclamation mark.'''
+    return [f"{element.capitalize()}!" for element in elements]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    '''Returns true if any calls are longer than 4 characters.'''
+    return any(len(call) > 4 for call in calls)
+
+def find_the_cheese(snacks):
+    '''Returns the first element of the array that is cheese.'''
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for snack in snacks:
+        if snack in cheeses:
+            return snack
+    return None
